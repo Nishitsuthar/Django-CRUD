775ae4c09f6a1b5fddcb46f7954e266e123b1e70
jon <nishitsuthar123@gmail.com>
2021-06-11 18:00:48 +0530

    make it better

--- crud/settings.py
+++ crud/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-4hst0j1c*-t99lyio*g@9v8xo(eu=8-kthr)@%jv-_fgcx0q-5"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["employee-crud15.herokuapp.com", "localhost"]
 
